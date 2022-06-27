c359328d5fd84f910dee09e83f65139ac48ff342
Laura Thomas <lthomas@cedarrapids.org>
2022-06-27 09:58:20 -0500

    added comments!

--- app.R
+++ app.R
@@ -39,7 +39,7 @@ ui <- fluidPage(
                                label = "Select counties:",
                                choices = c("Benton", "Linn", "Jones", "Johnson", "Washington", "Cedar", "Iowa"),
                                selected = c("Benton", "Linn", "Jones"))),
-              column(6,
+              column(6, # TO DO # this stuff overflows when the window is too narrow
                      actionButton("CRMSA", "Cedar Rapids, IA MSA"),
                      br(),
                      br(),
@@ -70,9 +70,12 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
+  # make variables for date range values reactive
   react_begOfMonth <- reactive({floor_date(input$daterange[1], "month")})
   react_endOfMonth <- reactive({ceiling_date(input$daterange[2], "month") - 1})
   
+  
+  # this was just test code for debugging
   output$testing2 <- renderPrint({
  print(c(react_begOfMonth(), react_endOfMonth()))
   
@@ -81,16 +84,22 @@ server <- function(input, output, session) {
   # https://shiny.rstudio.com/reference/shiny/0.14/updateCheckboxGroupInput.html
   # https://shiny.rstudio.com/articles/action-buttons.html
   
+  # clicking CR MSA button will select respective counties
   observeEvent(input$CRMSA, {
     updateCheckboxGroupInput(session, "counties",
                              selected = c("Benton", "Linn", "Jones"))
   })
   
+  
+  # clicking IC MSA button will select respective counties
   observeEvent(input$ICMSA, {
     updateCheckboxGroupInput(session, "counties",
                              selected = c("Johnson", "Washington"))
   })
   
+  # https://stackoverflow.com/questions/31139791/checkboxgroupinput-set-minimum-and-maximum-number-of-selections-ticks
+  # also want to add a pop-up message to alert user that they need to select at least one county
+  # or I could program the charts to wait until something is selected before drawing
   observe({
     if(length(input$counties) < 1){
       updateCheckboxGroupInput(session, "counties",
@@ -100,12 +109,10 @@ server <- function(input, output, session) {
 
  
   
-  sum_fun <- function(x){
-    return(data.frame(y = sum(x), label = sum(x)))
-  }
+
   
   output$LFgraph <- renderPlot({
-      # the way I did it before the reactive expressions above
+      # the way I included reactive expression variables before the reactive expressions above
       # begOfMonth <- floor_date(input$daterange[1], "month")
       # endOfMonth <- ceiling_date(input$daterange[2], "month") - 1
       dataset <- get(input$metric)
@@ -123,12 +130,12 @@ server <- function(input, output, session) {
           # add a point at the end of the line
         stat_summary(na.rm = TRUE, fun = "sum", geom = "point", size = 5, 
                      data = dataForStatSumm) +
-        # these lines are for debugging
-          stat_summary(na.rm = TRUE, fun = "sum", geom = "point", size = 5,
-                       data = 
-                         filter(dataset, date >= react_begOfMonth() & date <= react_endOfMonth()) %>% 
-                         filter(date == react_begOfMonth()) %>%
-                         filter(county %in% input$counties)) +
+        # these lines are for debugging # adds a point to the beginning
+          # stat_summary(na.rm = TRUE, fun = "sum", geom = "point", size = 5,
+          #              data = 
+          #                filter(dataset, date >= react_begOfMonth() & date <= react_endOfMonth()) %>% 
+          #                filter(date == react_begOfMonth()) %>%
+          #                filter(county %in% input$counties)) +
           # end debugging
           
         # labels the last point
@@ -141,7 +148,7 @@ server <- function(input, output, session) {
                      date_labels = "%b %Y",
                      expand = expansion(mult = c(0,0.15))) + # https://www.rdocumentation.org/packages/ggplot2/versions/3.3.6/topics/expansion
         scale_y_continuous(labels = scales::comma) +
-          ggtitle(unique(dataset$metric_name))
+          ggtitle(unique(dataset$metric_name)) # might want to pull this out into a reactive function
           
                 
     })
@@ -157,9 +164,9 @@ server <- function(input, output, session) {
           grepl("5$", series_id) ~ "Employment",
           grepl("4$", series_id) ~ "Unemployment"
         )) %>% 
-        mutate(date = as.Date(as.character(date)))
+        mutate(date = as.Date(as.character(date))) # this might not be necessary # TO DO # test removing
       
-      # This reverses the metrics in the chart.
+      # This reverses the metrics in the chart so employment shows on the bottom.
       stackeddata$metricname <- as.factor(stackeddata$metricname) %>% fct_rev()
       
       # This is the first step in adjusting the y axes so it's easier to see the nuances in the data better
@@ -175,6 +182,7 @@ server <- function(input, output, session) {
       # This is the second step that takes 95% of the lowest value to add some buffer.
       lowestEmp <- y_axis_min * .95
       
+      # TO DO # pretty this up more, add labels, switch colors
       ggplot(
         stackeddata %>% 
           filter(!grepl("6$", series_id)) %>% # Filter out total labor force
@@ -190,7 +198,7 @@ server <- function(input, output, session) {
         coord_cartesian(ylim = c(lowestEmp,NA))
         # stat_summary(na.rm = TRUE, fun = "sum", geom = "text", 
         #              aes(label = date), # https://stackoverflow.com/questions/56490813/how-do-i-add-a-comma-separator-to-a-text-label-in-geom-text
-        #              size = 5, angle = 90)
+        #              size = 5, angle = 90) # only geom text lets you set the angle
     
     })
     
